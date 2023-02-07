23b608d60f12f1d2b62fecbf651b9d7293068ead
kluskaj <11367352+kluskaj@users.noreply.github.com>
2023-02-07 17:06:16 +0100

    change version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='postOrganic',
-    version='0.0.1',
+    version='0.0.2',
     entry_points={
         'console_scripts': [
             'postOrganic=postOrganic:main'
